163d2387e6be3ef45cb35ff8a682fc34acb9bfa9
Yusuf <yusuf.kilic.sw@gmail.com>
2025-11-29 01:47:12 +0300

    created project

---
+++ src/main/java/com/example/demo/GitHubApacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubApacheApplication.class, args);
+	}
+
+}
